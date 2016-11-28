cc11341f4499701b2f662c3f848d5802a9e47416
kvasenko <kvasenko@ukr.net>
2016-11-28 18:42:21 +0200

    Added migrations

---
+++ src/migrations/20161128115221_create_test_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateTestTable extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->execute('INSERT INTO `acl_privileges` (`roleId`, `module`, `privilege`) VALUES (\'2\',\'test\',\'Create\'),(\'2\',\'test\',\'Delete\'),(\'2\',\'test\',\'Read\'),(\'2\',\'test\',\'Update\');');
+        $this->execute('CREATE TABLE `test` (`id` bigint(20) NOT NULL AUTO_INCREMENT, `name` varchar(255) DEFAULT NULL, `email` varchar(512) DEFAULT NULL, `status` enum(\'active\',\'disable\',\'delete\') DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;');
+        $this->execute('INSERT INTO `test`(`id`,`name`,`email`,`status`) VALUES (10,\'Jonah\',\'dictum@pharetra . ca\',\'disable\'),(11,\'Connor\',\'congue . In . scelerisque@Integervulputaterisus . ca\',\'disable\');');
+    }
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('test');
+        $this->execute('DELETE FROM `auth` WHERE foreignKey=\'test\'');
+        $this->execute('DELETE FROM `acl_privileges` WHERE module=\'test\'');
+    }
+}
