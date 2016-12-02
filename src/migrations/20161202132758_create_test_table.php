08d8d08f8d136345ae36f210da797a59b9314db6
kvasenko <kvasenko@ukr.net>
2016-12-02 16:09:22 +0200

    update migration

--- src/migrations/20161202132758_create_test_table.php
+++ src/migrations/20161202132758_create_test_table.php
@@ -9,9 +9,9 @@ class CreateTestTable extends AbstractMigration
      */
     public function up()
     {
-        $this->execute('INSERT INTO `acl_privileges` (`roleId`, `module`, `privilege`) VALUES (\'2\',\'test\',\'Create\'),(\'2\',\'test\',\'Delete\'),(\'2\',\'test\',\'Read\'),(\'2\',\'test\',\'Update\');');
+        $this->execute('REPLACE INTO `acl_privileges` (`roleId`, `module`, `privilege`) VALUES (\'2\',\'test\',\'Create\'),(\'2\',\'test\',\'Delete\'),(\'2\',\'test\',\'Read\'),(\'2\',\'test\',\'Update\');');
         $this->execute('CREATE TABLE `test` (`id` bigint(20) NOT NULL AUTO_INCREMENT, `name` varchar(255) DEFAULT NULL, `email` varchar(512) DEFAULT NULL, `status` enum(\'active\',\'disable\',\'delete\') DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;');
-        $this->execute('INSERT INTO `test`(`id`,`name`,`email`,`status`) VALUES (10,\'Jonah\',\'dictum@pharetra . ca\',\'disable\'),(11,\'Connor\',\'congue . In . scelerisque@Integervulputaterisus . ca\',\'disable\');');
+        $this->execute('REPLACE INTO `test`(`id`,`name`,`email`,`status`) VALUES (10,\'Jonah\',\'dictum@pharetra . ca\',\'disable\'),(11,\'Connor\',\'congue . In . scelerisque@Integervulputaterisus . ca\',\'disable\');');
     }
     /**
      * Migrate Down.
