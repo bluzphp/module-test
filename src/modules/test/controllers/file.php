c7affebec2000cc93a1512bd2bc07f5718901d15
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-01-30 14:39:19 +0200

    Uses `attachment($file)` helper for `Controller`

--- src/modules/test/controllers/file.php
+++ src/modules/test/controllers/file.php
@@ -13,23 +13,16 @@ use Bluz\Controller\Controller;
 use Bluz\Proxy\Response;
 
 /**
- * FIXME: closure should be back...
- * @return \closure
+ * @return void
  */
 return function () {
     /**
      * @var Controller $this
      */
-    $this->useLayout(false);
-
     Response::addHeader('Content-Description', 'File Transfer');
-    Response::addHeader('Content-Type', 'application/octet-stream');
-    Response::addHeader('Content-Disposition', 'attachment; filename=loading.gif');
     Response::addHeader('Expires', '0');
     Response::addHeader('Cache-Control', 'must-revalidate');
     Response::addHeader('Pragma', 'public');
 
-    return function () {
-        readfile(PATH_PUBLIC.'/img/loading.gif');
-    };
+    $this->attachment(PATH_PUBLIC.'/img/loading.gif');
 };
